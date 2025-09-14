8f342751bee33ab6808b2a080fef0264a2eb49e2
Gabriel Roberto <gabreil.zafani@gmail.com>
2025-09-14 20:46:14 -0300

    Paciente
    classe paciente

---
+++ Estrutura de dados/src/Paciente.java
@@ -0,0 +1,52 @@
+public class Paciente {
+    private static int contadorId = 0;
+    private int id;
+    private String nome;
+    private int idade;
+    private String sintoma;
+
+    public Paciente(String nome, int idade, String sintoma) {
+        this.id = ++contadorId;
+        this.nome = nome;
+        this.idade = idade;
+        this.sintoma = sintoma;
+    }
+
+    // Getters e Setters
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public int getIdade() {
+        return idade;
+    }
+
+    public void setIdade(int idade) {
+        this.idade = idade;
+    }
+
+    public String getSintoma() {
+        return sintoma;
+    }
+
+    public void setSintoma(String sintoma) {
+        this.sintoma = sintoma;
+    }
+
+
+    public String exibirInfo() {
+        return "ID: " + this.id + " | Nome: " + this.nome + " | Idade: " + this.idade + " | Sintoma: " + this.sintoma;
+    }
+}
